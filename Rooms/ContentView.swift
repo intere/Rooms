bb64959612c5972264f72e3ca3981b44c4fb0e7a
Eric Internicola <intere@gmail.com>
2019-06-10 15:43:37 -0600

    Initial Commit

---
+++ Rooms/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Rooms
+//
+//  Created by Eric Internicola on 6/10/19.
+//  Copyright © 2019 iColasoft. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
