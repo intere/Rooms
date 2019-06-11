2e595c2bff0f30c9c1f52c995234e82e7333c1a6
Eric Internicola <intere@gmail.com>
2019-06-10 21:32:23 -0600

    Added some more features.

--- Rooms/RoomDetail.swift
+++ Rooms/RoomDetail.swift
@@ -12,21 +12,40 @@ struct RoomDetail : View {
     let room: Room
     @State private var zoomed = false
 
+    private var showVideo: Bool {
+        room.hasVideo && !zoomed
+    }
+
     var body: some View {
-        Image(room.imageName)
-            .resizable()
-            .aspectRatio(contentMode: zoomed ? .fill : .fit)
-            .navigationBarTitle(Text(room.name), displayMode: .inline)
-            .tapAction { self.zoomed.toggle() }
+        ZStack(alignment: .topLeading) {
+            Image(room.imageName)
+                .resizable()
+                .aspectRatio(contentMode: zoomed ? .fill : .fit)
+                .navigationBarTitle(Text(room.name), displayMode: .inline)
+                .tapAction {
+                    withAnimation(.basic(duration: 2)) {
+                        self.zoomed.toggle()
+                    }
+                }
+                .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
+            if showVideo {
+                Image(systemName: "video.fill")
+                    .font(.title)
+                    .padding(.all)
+                    .transition(.move(edge: .leading))
+            }
+        }
     }
 }
 
 #if DEBUG
 struct RoomDetail_Previews : PreviewProvider {
     static var previews: some View {
-        NavigationView {
-            RoomDetail(room: testData[0])
+        Group {
+            NavigationView { RoomDetail(room: testData[0]) }
+            NavigationView { RoomDetail(room: testData[1]) }
         }
+
     }
 }
 #endif
